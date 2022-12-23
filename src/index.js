55258263c70fb71a02a8260c6985b7c7549d4983
Ihor Radchenko <103518662+DisRachik@users.noreply.github.com>
2022-12-23 21:46:12 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+// Tarabukin Mykkola - header
+import './js/header/header-js';
+import './js/header/header_mobile_menu';
+import './js/header/header_modal';
+
 // Tetiana Ustumenko franchise-modal
-import './js/mod-franchise'
+import './js/mod-franchise';
+// Ustymenko Tetiana - location-modal
+import './js/mod-locations';
