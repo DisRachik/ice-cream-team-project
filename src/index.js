e924ea2c1fb49e29596848882057f8ce994496ab
DisRachik <radchenkoig@gmail.com>
2022-12-24 12:40:44 +0200

    re-added header after revert

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-// Ustymenko Tetiana - location-modal
-import './js/mod-locations';
+// Tarabukin Mykkola - header
+import './js/header/header-js';
+import './js/header/header_mobile_menu';
+import './js/header/header_modal';
 
+// Tetiana Ustumenko franchise-modal
+import './js/mod-franchise';
+// Ustymenko Tetiana - location-modal
 import './js/mod-locations';
