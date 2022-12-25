309219cfb236e86c41c3e57da1f7e517724837bb
Tetiana Pesotska <verhulev81@gmail.com>
2022-12-25 15:09:29 +0200

    add changes scss

--- src/js/mod-about.js
+++ src/js/mod-about.js
@@ -1,8 +1,8 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
+      openModalBtn: document.querySelector("[data-mod-about-open]"),
+      closeModalBtn: document.querySelector("[data-mod-about-close]"),
+      modal: document.querySelector("[data-mod-about]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
