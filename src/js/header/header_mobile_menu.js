5c214732ed55d43627534b4f1d303973337c460f
DisRachik <radchenkoig@gmail.com>
2022-12-27 23:17:20 +0200

    added modification scroll

--- src/js/header/header_mobile_menu.js
+++ src/js/header/header_mobile_menu.js
@@ -1,7 +1,6 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-header-menu-button]');
   const mobileMenuRef = document.querySelector('[data-header-menu]');
-  const bodyScrollLock = document.querySelector('[body-scroll-lock]');
 
   menuBtnRef.addEventListener('click', () => {
     const expanded =
@@ -10,7 +9,6 @@
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
-    bodyScrollLock.classList.toggle('is-open');
     mobileMenuRef.classList.toggle('is-open');
   });
 })();
