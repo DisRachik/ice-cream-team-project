661fd6c63269c49dc01288a3579e2f86080e963a
DisRachik <radchenkoig@gmail.com>
2022-12-27 15:17:51 +0200

    fixed mobile-menu

--- src/js/header/header_mobile_menu.js
+++ src/js/header/header_mobile_menu.js
@@ -1,6 +1,7 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-header-menu-button]');
   const mobileMenuRef = document.querySelector('[data-header-menu]');
+  const bodyScrollLock = document.querySelector('[body-scroll-lock]');
 
   menuBtnRef.addEventListener('click', () => {
     const expanded =
@@ -9,6 +10,7 @@
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
+    bodyScrollLock.classList.toggle('is-open');
     mobileMenuRef.classList.toggle('is-open');
   });
 })();
